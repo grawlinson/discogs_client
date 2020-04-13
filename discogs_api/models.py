6414a56a0be5820b1759cc5366e6775af426ca35
Clement T <me@trosa.io>
2020-04-13 10:52:25 +0200

    Added new entry in models

--- discogs_api/models.py
+++ discogs_api/models.py
@@ -525,6 +525,7 @@ class Label(PrimaryAPIObject):
     id = SimpleField()
     name = SimpleField()
     profile = SimpleField()
+    catno = SimpleField()
     urls = SimpleField()
     images = SimpleField()
     contact_info = SimpleField()
