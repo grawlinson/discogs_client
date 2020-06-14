12f596607e5510184dbdb9d99ed9d04893068756
George Rawlinson <george@rawlinson.net.nz>
2020-06-14 19:58:45 +1200

    release: 0.1.0
    Merged a few more PRs from upstream, at least one has a breaking change.
    
    The function 'OAuth2Fetcher' has been renamed to 'OAuth1Fetcher', the
    function signature remains the same.

--- discogs_api/__init__.py
+++ discogs_api/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-__version__ = '0.0.3'
+__version__ = '0.1.0'
 
 from discogs_api.client import Client
 from discogs_api.models import Artist, Release, Master, Label, User, \
